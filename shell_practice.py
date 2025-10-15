928a3a2e0af42e4c7c7e2614b54277e320f4c9a8
Arish <arishm1000@gmail.com>
2025-10-15 18:18:14 +0500

    fix shell commands file

--- shell_practice.py
+++ shell_practice.py
@@ -1,61 +1,46 @@
 from datetime import date
+from django.db.models import Count
 
 from banks.models import Bank, BankBranch
 from users.models import User
 from accounts.models import BankAccount
 
 
-# Create
-bank = Bank.objects.create(
-    name="Meezan Bank",
-    swift_code="MEZNPKKA",
-    is_islamic=True,
-    established_date=date(1997, 1, 1)
-)
+# Create banks
+banks = [
+    {"name": "ABC Bank", "swift_code": "ABC12345678", "is_islamic": False, "established_date": date(1990, 5, 12)},
+    {"name": "XYZ Islamic Bank", "swift_code": "XYZ87654321", "is_islamic": True, "established_date": date(2000, 3, 20)},
+    {"name": "National Bank", "swift_code": "NAT11223344", "is_islamic": False, "established_date": date(1985, 7, 15)},
+]
+for bank in banks:
+    Bank.objects.create(**bank)
 
-bank = Bank.objects.create(
-    name="MCB",
-    swift_code="MEZNPKHH",
-    is_islamic=True,
-    established_date=date(1997, 1, 1)
-)
-
-bank = Bank.objects.create(
-    name="Alflah",
-    swift_code="MEZNPKYY",
-    is_islamic=True,
-    established_date=date(1991, 1, 1)
-)
-
-
+# Create Branches
+banks = Bank.objects.all()
+for bank in banks:
+    branches_data = [
+        {"name": f"{bank.name} Main Branch", "branch_code": "001", "address": "Main Street 1"},
+        {"name": f"{bank.name} City Branch", "branch_code": "002", "address": "City Center 5"},
+        {"name": f"{bank.name} Suburb Branch", "branch_code": "003", "address": "Suburb Road 10"},
+    ]
+    for branch_data in branches_data:
+        BankBranch.objects.create(bank=bank, **branch_data)
 
 # Filter
 islamic_banks = Bank.objects.filter(is_islamic=True)
 non_islamic_banks = Bank.objects.filter(is_islamic=False)
 and_query = Bank.objects.filter(is_islamic=True, name="Meezan Bank")
 
-
-
 # Filter Islamic banks with active accounts
 banks = Bank.objects.filter(is_islamic=True, branches__branch_accounts__is_active=True)
-
-
 
 # Get
 bank = Bank.objects.get(name="Meezan Bank")
 bank = Bank.objects.get(pk=1)
 
-
-
-
 # Values
-
-# get all the fields
 banks = Bank.objects.values()
-
-# get the name column only
 banks = Bank.objects.values('name')
-
 
 # Select_related
 branches = BankBranch.objects.select_related('bank')
@@ -63,15 +48,10 @@ accounts = BankAccount.objects.select_related('user')
 accounts = BankAccount.objects.select_related('branch')
 accounts = BankAccount.objects.select_related('user', 'branch')
 
-
-
 # Prefetch_related
 banks = Bank.objects.prefetch_related('branches')
 banks = Bank.objects.prefetch_related('branches', 'branches__branch_accounts')
 users = User.objects.prefetch_related('user_accounts')
-
-
-
 
 # Annotate
 banks = Bank.objects.annotate(
